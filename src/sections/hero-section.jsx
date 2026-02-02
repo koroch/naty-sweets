67164396776190c73b7b2014383390731ed4ab6c
koroch <joantanaguiar1998@hotmail.com>
2026-02-02 12:37:34 -0300

    add/import-new-font-to-title

--- src/sections/hero-section.jsx
+++ src/sections/hero-section.jsx
@@ -4,24 +4,14 @@ import SectionTitle from "../components/section-title";
 
 export default function HeroSection() {
     return (
-        <section className="flex flex-col items-center -mt-18">
-            
-            <motion.h1 className="text-center text-5xl leading-[68px] md:text-6xl md:leading-[70px] mt-4 font-semibold max-w-2xl"
-                initial={{ y: 50, opacity: 0 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{ type: "spring", stiffness: 240, damping: 70, mass: 1 }}
-            >
-            <SectionTitle className="" title="Seja bem-vindo(a) à Naty__Sweets" description="" />
-                
-            </motion.h1>
+        <section className="font-lobster flex flex-col items-center -mt-18">
             <motion.p className="text-center text-base max-w-lg mt-2"
                 initial={{ y: 50, opacity: 0 }}
                 whileInView={{ y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 transition={{ delay: 0.2, type: "spring", stiffness: 320, damping: 70, mass: 1 }}
             >
-                Doceria Gourmet Online e doces artesanais
+                <SectionTitle className="" title="Seja bem-vindo(a) à Naty__Sweets" description="Doceria Gourmet Online e doces artesanais" />
             </motion.p>
             
             <TiltedImage />
